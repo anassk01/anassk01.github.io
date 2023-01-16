b6412700e3ec1c013024b47d6600dcdfef83c167
anassk01 <anaskabil01@gmail.com>
2023-01-16 08:08:46 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const noteContainer = document.getElementById("note-container");
+const bookTitle = document.getElementById("book-title");
+
+fetch("data.json")
+.then(res => res.json())
+.then(data => {
+  bookTitle.innerHTML = data.bookName;
+  data.noteList.forEach(note => {
+    let level = note.color === 2 ? 1 : note.color === 4 ? 2 : 3;
+    const noteElement = document.createElement("div");
+    noteElement.classList.add("note");
+    noteElement.innerHTML = `<h${level} class="note-title level-${level}" style="background-color:${note.bgColor}">${note.text}</h${level}>
+    <p class="note-content level-${level}" style="display:none">${note.note}</p>`;
+    noteContainer.appendChild(noteElement);
+  });
+  const noteTitles = document.querySelectorAll(".note-title");
+  noteTitles.forEach(title => {
+    title.addEventListener("click", e => {
+      e.target.nextElementSibling.style.display = e.target.nextElementSibling.style.display === "none" ? "block" : "none";
+    });
+  });
+});
